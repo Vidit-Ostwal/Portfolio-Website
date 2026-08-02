c58af9bccf2f90201d2e654e621a7f1f8059d3aa
Vidit-Ostwal <viditostwal@gmail.com>
2026-08-02 12:34:13 +0530

    Dropping unused assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://vidit-ostwal.github.io',
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
